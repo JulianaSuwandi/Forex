bb363b193117ea739d3613520da01761a2ef4020
feniaster <92568120+JulianaSuwandi@users.noreply.github.com>
2023-06-22 10:02:33 +0700

    Initial commit

---
+++ app/src/main/java/com/juliana212102441/forex/RootModel.java
@@ -0,0 +1,18 @@
+package com.juliana212102441.forex;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RootModel {
+    @SerializedName("rates")
+    private RatesModel ratesModel;
+
+    public RootModel(){}
+
+    public RatesModel getRatesModel() {
+        return ratesModel;
+    }
+
+    public void setRatesModel(RatesModel ratesModel) {
+        this.ratesModel = ratesModel;
+    }
+}
